a2b394937bed4c6105d3f8ac76fc94814ff98389
MaximKirko <30max30max@gmail.com>
2016-10-22 00:24:55 +0300

    update db

--- db/library_postgres_create.sql
+++ db/library_postgres_create.sql
@@ -41,6 +41,7 @@ CREATE TABLE "test_2_question" (
 CREATE TABLE "subject" (
 	"id" serial NOT NULL,
 	"name" character varying(128) NOT NULL UNIQUE,
+	"description" character varying(512),
 	CONSTRAINT subject_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -57,6 +58,37 @@ CREATE TABLE "test_2_subject" (
 
 
 
+CREATE TABLE "user" (
+	"id" serial NOT NULL,
+	"first_name" character varying(256) NOT NULL,
+	"last_name" character varying(256) NOT NULL,
+	CONSTRAINT user_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "user_details" (
+	"id" serial NOT NULL,
+	"email" character varying(256) NOT NULL UNIQUE,
+	"password" character varying(256) NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "grades" (
+	"user_id" bigint NOT NULL,
+	"test_id" bigint NOT NULL,
+	"mark" bigint NOT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
 
 ALTER TABLE "question" ADD CONSTRAINT "question_fk0" FOREIGN KEY ("answer_id") REFERENCES "answer"("id");
 
@@ -67,3 +99,9 @@ ALTER TABLE "test_2_question" ADD CONSTRAINT "test_2_question_fk1" FOREIGN KEY (
 
 ALTER TABLE "test_2_subject" ADD CONSTRAINT "test_2_subject_fk0" FOREIGN KEY ("test") REFERENCES "test"("id");
 ALTER TABLE "test_2_subject" ADD CONSTRAINT "test_2_subject_fk1" FOREIGN KEY ("subject") REFERENCES "subject"("id");
+
+
+ALTER TABLE "user_details" ADD CONSTRAINT "user_details_fk0" FOREIGN KEY ("id") REFERENCES "user"("id");
+
+ALTER TABLE "grades" ADD CONSTRAINT "grades_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
+ALTER TABLE "grades" ADD CONSTRAINT "grades_fk1" FOREIGN KEY ("test_id") REFERENCES "test"("id");
